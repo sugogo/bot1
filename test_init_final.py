f3850ea5f78deb382dff2ac7816549ae1f12fe40
sugogo <57973694+sugogo@users.noreply.github.com>
2019-11-20 12:18:41 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -199,8 +199,8 @@ def init():
 			endTime = endTime + datetime.timedelta(days=int(basicSetting[13]))
 
 	### 채널 고정###
-	#basicSetting[6] = int('597781866681991198') #보이스채널ID
-	#basicSetting[7] = int('597782016607649829') #택스트채널ID
+	#basicSetting[6] = int('633958120502067219') #보이스채널ID
+	#basicSetting[7] = int('633958316707545088') #택스트채널ID
 	
 	bossNum = int(len(boss_inputData)/5)
 
